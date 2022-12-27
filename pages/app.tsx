658b309cbc9e91f14414c8478d47cb7eab649e87
johnmiicheal <michealelijah301@gmail.com>
2022-12-27 17:01:48 +0100

    fix empty files

---
+++ pages/app.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { Text, Center } from "@chakra-ui/react";
+
+export const MyApp = () => {
+    return(
+        <Center>
+            <Text> Dashboard here </Text>
+        </Center>
+    )
+}
--- pages/audio.tsx
+++ pages/audio.tsx
@@ -1,8 +1,10 @@
 import React from "react";
-
+import { Text, Center } from "@chakra-ui/react";
 
 export const Audio = () => {
     return(
-        <div> Audio </div>
+        <Center>
+            <Text> ChatGPT and Whisper Chat together </Text>
+        </Center>
     )
 }
