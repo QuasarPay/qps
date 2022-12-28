98753f92ff40f45831f1a70e5fdbd44925bc7ea0
johnmiicheal <michealelijah301@gmail.com>
2022-12-28 11:35:36 +0100

    [god-mode activated L2]

---
+++ pages/404.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Text, Center } from "@chakra-ui/react";
+
+const Custom404 = () => {
+    return(
+        <Center>
+            <Text mt={40}> Could not find what you asked for, lol </Text>
+        </Center>
+    )
+}
+
+export default Custom404
